50768411e34171159d9f9f165c949e49c407f234
Tennyson Hinds <StlTenny@gmail.com>
2012-03-14 14:44:59 -0400

    Example changed to reflect headers.

--- src/lu/thinds/android/RestServiceActivity.java
+++ src/lu/thinds/android/RestServiceActivity.java
@@ -17,19 +17,22 @@ public class RestServiceActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.testrest);
         
-        restService = new RestService(mHandler, this, "http://10.0.2.2:3000/bars/search");
-        restService.addParam("lat", "40764917");
+        restService = new RestService(mHandler, this, "http://10.0.2.2:3000/bars/");
+        restService.addParam("lat", "40764917");  //Add params to request
         restService.addParam("lng", "-73983130");
         restService.addParam("range", "10");
+        restService.addHeader("Content-Type","application/json");//Add headers to request
         
-        t_query1 = (TextView) findViewById(R.id.t_query1);
+        
+        
+        t_query1 = (TextView) findViewById(R.id.t_query1); //Setup Views and Button for response
         t_query1.setMovementMethod(new ScrollingMovementMethod());
         
         Button b_query1 = (Button) findViewById(R.id.b_query1);
     	b_query1.setOnClickListener(new View.OnClickListener(){
     		public void onClick(View view){
     			try {
-					restService.execute(RestService.GET);
+					restService.execute(RestService.GET); //Executes the request with the HTTP GET verb
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -38,6 +41,7 @@ public class RestServiceActivity extends Activity{
     	
     }
     
+    //Overridden handler to process incoming response. Response string is attached as msg.obj.
     private final Handler mHandler = new Handler(){
     	@Override
     	public void handleMessage(Message msg){
